f70ebcb7a617e88726b64cb274a09dd4e1d7d3c8
Laraib-Khalid <laraibkhalid7861@gmail.com>
2025-02-27 14:52:11 +0500

    This is the Changing code that includes commented text.

--- base_pages/Recruitment_Admin_Page.py
+++ base_pages/Recruitment_Admin_Page.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from test_cases.TC1_admin_login import Test_01_Admin_login
 
-
+# This is the Recruitment Admin Page Class Code
 class Recruitment_Admin_Page:
     admin_page_url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     textbox_first_name = "mian"
